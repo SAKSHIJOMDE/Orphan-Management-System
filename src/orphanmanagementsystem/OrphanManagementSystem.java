21dd2c37279f80b1c12858b2d378b9b150f822b6
ROSHAN-KHANDAGALE <roshan15khandagale@gmail.com>
2023-04-14 12:44:44 +0530

    First Commit

---
+++ src/orphanmanagementsystem/OrphanManagementSystem.java
@@ -0,0 +1,10 @@
+package orphanmanagementsystem;
+
+public class OrphanManagementSystem {
+
+    public static void main(String[] args) {
+        DashBoard dash = new DashBoard();
+        dash.setVisible(true);
+    }
+    
+}
