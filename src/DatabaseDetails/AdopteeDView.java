bf2510b7e701009c4e37e05f14d9193978082ab7
SAKSHI-JOMDE30 <jomdesakshi3130@gmail.com>
2023-04-18 00:25:34 +0530

    SAKSHI COMMITED

--- src/DatabaseDetails/AdopteeDView.java
+++ src/DatabaseDetails/AdopteeDView.java
@@ -326,7 +326,7 @@ public class AdopteeDView extends javax.swing.JFrame {
                         .addComponent(REFRESHB)
                         .addGap(18, 18, 18)
                         .addComponent(CLOSEB)))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 19, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addComponent(TablePanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
         );
         MainPanelLayout.setVerticalGroup(
